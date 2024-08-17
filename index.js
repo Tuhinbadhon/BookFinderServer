e680a7105d3ca251792696bf9e1b2065696f049a
Tuhinbadhon <tuhinbadhon@gmail.com>
2024-08-17 20:05:21 +0600

    get operation done

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 // mongodb cnctn
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cauvj2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cauvj2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,7 +34,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productsCollection = client.db("styleroomDB").collection("products");
+    const productsCollection = client.db("FHdb").collection("products");
 
     //getting data from mongodb
     app.get("/products", async (req, res) => {
@@ -51,9 +52,7 @@ async function run() {
       };
 
       try {
-        const productsCollection = client
-          .db("styleroomDB")
-          .collection("products");
+        const productsCollection = client.db("FHdb").collection("products");
         const totalProducts = await productsCollection.countDocuments(query);
         const totalPages = Math.ceil(totalProducts / limit);
 
